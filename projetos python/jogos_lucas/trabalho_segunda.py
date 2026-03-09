cbdaa6f9cc831a0e774d4e5f767732e0afc4b078
gabriellycama <gabrielly.camargo.pereira@escola.pr.gov.br>
2026-03-09 12:08:13 -0300

    Add files via upload

--- projetos python/jogos_lucas/trabalho_segunda.py
+++ projetos python/jogos_lucas/trabalho_segunda.py
@@ -1,3 +1,22 @@
-print("trabalho_teste")
- list item in  zip ([0,2,4,6])
- 
+
+import random
+
+print("bem vindo a o dado")
+dados = []
+
+for i in range(3):
+    ##repetir algo ou percorrer uma lista for in 
+    dados.append(random.randint(1,6))
+##quantidade de areas 1,6
+##evitando numeros inteiros
+print("Resultados", dados)
+
+soma = sum(dados)
+##sum soma todos os numeros da lista
+
+media = soma // len(dados)
+##conta quantos itens tem len
+##dividindo pelos numeros
+
+print("Soma:", soma)
+print("Média:", media)
